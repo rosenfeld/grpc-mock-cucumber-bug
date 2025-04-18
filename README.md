b108386514f474a98c7498603615a147d890a5bb
Rodrigo Rosenfeld Rosas <rodrigo@rosenfeld.page>
2025-04-18 11:19:21 -0300

    Add a fix demonstrating the issue

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Then change the grpc version to 1.65.0 or newer:
 
 You should notice the command will never exit.
 
+If we also set the `fix` ENV, we monkey patch `GRPC::ActiveCall` to fix the problem.
+
+The fix can be seen in `features/support/env.rb`.
+
+    GRPC_VERSION=1.71.0 bundle && GRPC_VERSION=1.71.0 fix=1 bin/cucumber
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,3 +3,17 @@ require 'bundler/setup'
 $LOAD_PATH << File.expand_path('../../lib', __dir__)
 
 require_relative 'grpc'
+
+if ENV["fix"]
+  module ActiveCallPatch
+    CALLS = []
+    def initialize(*args, **kargs, &block)
+      super
+      CALLS << @call
+    end
+  end
+
+  GRPC::ActiveCall.prepend ActiveCallPatch
+
+  at_exit { ActiveCallPatch::CALLS.each(&:close) }
+end
